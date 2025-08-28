b45f231e4be37e9dce82b740792ecc4748e28585
Pedro A. Neves <PedroANeves@users.noreply.github.com>
2025-08-28 00:00:00 +0000

    ref: moved assert to utils

--- tests/run.sh
+++ tests/run.sh
@@ -8,29 +8,28 @@ die() { echo "$*" 1>&2 ; exit 1; }
 
 source git-privacy
 
-# tests location setup
+source tests/utils
+
+# SETUP
 rm -fr tests/playground
 mkdir -p tests/playground
-
-# old commit with timestamp
 pushd tests/playground > /dev/null || exit
+
+# PREPARE
+# old commit with timestamp
 git init
 GIT_AUTHOR_DATE=2025-05-05T05:05:05+0500 \
   GIT_COMMITTER_DATE=2026-06-06T06:06:06+0600 \
   git commit --allow-empty -m 'old commit'
 
+# ACT
 # git privacy redact
 redact
 actual=$(git log --pretty=format:"%ai|%ci|%s")
+
+# TEARDOWN
 popd > /dev/null || exit
 
-# assert
+# ASSERT
 expected="2025-05-05 00:00:00 +0000|2026-06-06 00:00:00 +0000|old commit"
-if [[ "$actual" == "$expected" ]]; then
-  echo OK
-  exit 0
-else
-  echo "expected: $expected"
-  echo "actual  : $actual"
-  exit 1
-fi
+assert "$expected" "$actual"
